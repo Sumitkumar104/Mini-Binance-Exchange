2a836c2ea64c9a070ee64b8d77df340fd6c066b2
Sumitkumar104 <parjapatsumit104@gmail.com>
2024-07-04 22:27:57 +0530

    add real time depth

--- apps/frontend/app/components/depth/Depth.tsx
+++ apps/frontend/app/components/depth/Depth.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useState } from "react";
 import { getDepth, getKlines, getTicker, getTrades } from "../../utils/httpClient";
 import { BidTable } from "./BidTable";
@@ -7,21 +5,55 @@ import { AskTable } from "./AskTable";
 import { SignalingManager } from "@/app/utils/SignalingManager";
 
 export function Depth({ market }: {market: string}) {
-
-    const [bids, setBids] = useState<[string, string][]>();       // store the  price and quantity of the bids and update in realTime
-    const [asks, setAsks] = useState<[string, string][]>();      // store the  price and quantity of the asks and update in realTime
-    const [price, setPrice] = useState<string>();    // store the current price of the market
+    const [bids, setBids] = useState<[string, string][]>();
+    const [asks, setAsks] = useState<[string, string][]>();
+    const [price, setPrice] = useState<string>();
 
     useEffect(() => {
-        getDepth(market).then(d => {
+        SignalingManager.getInstance().registerCallback("depth", (data: any) => {
+            
+            setBids((originalBids) => {
+                const bidsAfterUpdate = [...(originalBids || [])];
+
+                for (let i = 0; i < bidsAfterUpdate.length; i++) {
+                    for (let j = 0; j < data.bids.length; j++)  {
+                        if (bidsAfterUpdate[i][0] === data.bids[j][0]) {
+                            bidsAfterUpdate[i][1] = data.bids[j][1];
+                            break;
+                        }
+                    }
+                }
+                return bidsAfterUpdate; 
+            });
+
+            setAsks((originalAsks) => {
+                const asksAfterUpdate = [...(originalAsks || [])];
+
+                for (let i = 0; i < asksAfterUpdate.length; i++) {
+                    for (let j = 0; j < data.asks.length; j++)  {
+                        if (asksAfterUpdate[i][0] === data.asks[j][0]) {
+                            asksAfterUpdate[i][1] = data.asks[j][1];
+                            break;
+                        }
+                    }
+                }
+                return asksAfterUpdate; 
+            });
+        }, `DEPTH-${market}`);
+        
+        SignalingManager.getInstance().sendMessage({"method":"SUBSCRIBE","params":[`depth.${market}`]});
+
+        getDepth(market).then(d => {    
             setBids(d.bids.reverse());
             setAsks(d.asks);
         });
 
         getTicker(market).then(t => setPrice(t.lastPrice));
-        
-
-        
+        getTrades(market).then(t => setPrice(t[0].price));
+        return () => {
+            SignalingManager.getInstance().sendMessage({"method":"UNSUBSCRIBE","params":[`depth.200ms.${market}`]});
+            SignalingManager.getInstance().deRegisterCallback("depth", `DEPTH-${market}`);
+        }
     }, [])
     
     return <div>
@@ -30,7 +62,6 @@ export function Depth({ market }: {market: string}) {
         {price && <div>{price}</div>}
         {bids && <BidTable bids={bids} />}
     </div>
-
 }
 
 function TableHeader() {
