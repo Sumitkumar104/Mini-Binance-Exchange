962c8e52ee37b10297efe733a1f4c7f0e313f40f
Sumitkumar104 <parjapatsumit104@gmail.com>
2024-07-08 17:52:45 +0530

    create types for API-server

---
+++ apps/api-Server/src/types/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports,"__esModule",{value:true});      //This line ensures compatibility between CommonJS (used by Node.js) and ES6 module systems.
+// this line ensures that the resulting CommonJS module can be correctly imported using ES6 import syntax.
+
+exports.GET_DEPTH = exports.ON_RAMP = exports.CANCEL_ORDER = exports.CREATE_ORDER = void 0;
+exports.CREATE_ORDER = "CREATE_ORDER";
+exports.CANCEL_ORDER = "CANCEL_ORDER";
+exports.ON_RAMP = "ON_RAMP";
+exports.GET_DEPTH = "GET_DEPTH";
