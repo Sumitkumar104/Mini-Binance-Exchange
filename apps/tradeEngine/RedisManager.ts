bda173a0e04ad32cdf51aa7f1e7c2fa2e882c3e1
Sumitkumar104 <parjapatsumit104@gmail.com>
2024-07-09 12:36:41 +0530

    init the tadeEngine Server

---
+++ apps/tradeEngine/RedisManager.ts
@@ -0,0 +1,4 @@
+// create a redis client to do 3 task
+// 1. publish the message to apiServer pub/subs
+// 2. Pudh the  data in DataBase message queue
+
---
+++ apps/tradeEngine/index.ts
@@ -0,0 +1,27 @@
+import { createClient } from "redis";
+import { Engine } from "./trade/Engine";
+
+
+async function main() {
+
+    const engine = new Engine(); 
+    const redisClient = createClient();
+    await redisClient.connect();
+    redisClient.on("error", (err) => {
+        console.log("Error to connect with redis message queue-" + err);
+    });
+
+    while (true) {
+
+        // pop the message from the reids queue and process it in engine function.
+        const response = await redisClient.rPop("messages" as string)
+        if (!response) {
+
+        }  else {
+            engine.process(JSON.parse(response));
+        }        
+    }
+
+}
+
+main();
