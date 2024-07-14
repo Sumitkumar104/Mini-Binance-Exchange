dc82f893de9844b950b780a4e057e6991c2fc3e5
Sumitkumar104 <parjapatsumit104@gmail.com>
2024-07-14 16:17:42 +0530

    fix some bug in frontend

--- apps/frontend/app/components/SwapUI.tsx
+++ apps/frontend/app/components/SwapUI.tsx
@@ -35,7 +35,7 @@ export function SwapUI({ market }: {market: string}) {
                                 <input step="0.01" placeholder="0" className="h-12 rounded-lg border-2 border-solid border-baseBorderLight bg-[var(--background)] pr-12 text-right text-2xl leading-9 text-[$text] placeholder-baseTextMedEmphasis ring-0 transition focus:border-accentBlue focus:ring-0" type="text" value="134.38" />
                                 <div className="flex flex-row absolute right-1 top-1 p-2">
                                     <div className="relative">
-                                        <img src="/usdc.webp" className="w-6 h-6" />
+                                        {/* <img src="/usdc.webp" className="w-6 h-6" /> */}
                                     </div>
                                 </div>
                             </div>
@@ -49,7 +49,7 @@ export function SwapUI({ market }: {market: string}) {
                             <input step="0.01" placeholder="0" className="h-12 rounded-lg border-2 border-solid border-baseBorderLight bg-[var(--background)] pr-12 text-right text-2xl leading-9 text-[$text] placeholder-baseTextMedEmphasis ring-0 transition focus:border-accentBlue focus:ring-0" type="text" value="123" />
                             <div className="flex flex-row absolute right-1 top-1 p-2">
                                 <div className="relative">
-                                    <img src="/sol.webp" className="w-6 h-6" />
+                                    {/* <img src="/sol.webp" className="w-6 h-6" /> */}
                                 </div>
                             </div>
                         </div>
--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,9 +1,33 @@
 import Image from "next/image";
+import Link from 'next/link';
+import { SuccessButton } from "./components/core/Button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Landing page edit it to make it realistic
+    <main className="flex flex-col items-center justify-between p-24">
+      <Link href="/trade/SOL_USDC">
+        <SuccessButton > Solana (SOL_USDC)</SuccessButton>
+      </Link>
+      <br />
+      <br />
+      <Link href="/trade/BTC_USDC">
+        <SuccessButton >Bitcoin (BTC_USDC)</SuccessButton>
+      </Link>
+      <br />
+      <br />
+      <Link href="/trade/ETH_USDC">
+        <SuccessButton >Ethereum (ETH_USDC)</SuccessButton>
+      </Link>
+      <br />
+      <br />
+      <Link href="/trade/UNI_USDC">
+        <SuccessButton >Uniswap (UNI_USDC)</SuccessButton>
+      </Link>
+      <br />
+      <br />
+      <Link href="/trade/MATIC_USDC">
+        <SuccessButton >polygon (MATIC_USDC)</SuccessButton>
+      </Link>
     </main>
   );
 }
